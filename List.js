ef5b26404d3a4ad35c12997fb608654f87433876
Brandon <brandon.williams205@t-mobile.com>
2022-12-19 09:00:20 -0800

    Append without using tail prop

--- List.js
+++ List.js
@@ -1,11 +1,23 @@
 class List {
     constructor(){
         this.head = null
-        this.tail = null
     }
     push(node){
         node.next = this.head;
         this.head = node;
+    }
+    append(node){
+        const emptyList = !this.head;
+        if(emptyList){
+            this.head = node;
+            return;
+        }
+        let current = this.head;
+        const currentNextNodeIsEmpty = current.next === null;
+        while(!currentNextNodeIsEmpty){
+            current = current.next;
+        }
+        current.next = node;
     }
 
 }
@@ -16,21 +28,11 @@ class Node {
     }
 }
 
-
 module.exports = {
     List,
     Node
 };
 
-
-// const list = new List();
-// const node1 = new Node(1);
-// const node2 = new Node(2);
-// const node3 = new Node(3);
-// list.push(node1);
-// list.push(node2);
-// list.push(node3);
-// console.log(list);
 
 // SOLID principles
 // Single responsibility principle
