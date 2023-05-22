37e2f7d72982fa3bbe1230a884c7b3c9fa712fbb
Eric Wilson <eric.wilson72@t-mobile.com>
2023-05-22 15:40:15 -0700

    * List endpoints backed by a database; * docker compose

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const app = express()
 const port = process.env.PORT || 3000;
-import {router} from './routes.js';
+import { router } from './routes.js';
+
 
 app.use(express.json());
 
@@ -15,3 +16,14 @@ app.use('/list', router)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+// Required for CTRL+C to work in docker
+process.on('SIGINT', function() {
+  process.exit();
+});
+// Required for CTRL+C to work in docker-compose
+process.on('SIGTERM', function() {
+  process.exit();
+});
